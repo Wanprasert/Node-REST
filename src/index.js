3563cd707e7580dcf5108ce2965b474eba5e49a5
Your Name <you@example.com>
2023-08-15 09:38:11 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express()
 const port = process.env.PORT;
 
-app.get("/", (req) => {
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
